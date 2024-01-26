d049b23842c11acbe16098b540dd5051c2becef7
Cristian Danu <crstdanu@gmail.com>
2024-01-26 15:58:08 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+var map = L.map('map').setView([47.163183932477416, 27.56422601343492], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
